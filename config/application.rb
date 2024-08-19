677060ae8429287f210188f49d37a92ecd3e3d29
Lucas Marcelino <lucas_marcelino2000@hotmail.com>
2024-08-18 22:22:47 -0300

    HTML & CSS: add pt-br locale and fix some html texts

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,9 @@ module XmlReaderApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # Use PT-BR default locale
+    config.i18n.default_locale = :"pt-BR"
+
     # Don't generate system test files.
     config.generators.system_tests = nil
 
